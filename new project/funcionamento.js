2cfce0ae0bb9b0de240de59909cd7a4eb290f608
Xadson Lázaro Relógio <xadsonlazaro007@gmail.com>
2025-06-18 07:59:04 -1200

    Verificador de numeros
    Fiz um site que verifica se um numero é par ou impar

--- new project/funcionamento.js
+++ new project/funcionamento.js
@@ -1,17 +1,47 @@
 
 function verificar () { 
-
-    let txt1 = document.getElementById("Number") 
+    let txt1 = document.getElementById("txt1") 
+    let res = document.getElementById("res")  
 
     if (txt1.value.length === 0) { 
-        window.alert('Ero verifique os Dados e tente Novamente')
-    } else { 
-        txt1 = Number(); 
-
-        if (Number % 2 === 0) {  
-            console.log(`O Numero ${txt1} È par`) 
+        let name = window.prompt(`Ola qual seu nome?`) 
+        window.alert(` Por favor ${name} Verifique os Dados e Tente Novamente!`)
+    } else {      
+        let n = Number(txt1.value); 
+        
+        if (n % 2 === 0) { 
+            res.innerHTML = `O Numero ${n} é um numero Par`
              } else { 
-                console.log(`O Numero ${txt1} é Impar`)
-             }
+                res.innerHTML = `O Numero ${n} È um numero impar`
         }
+         
     }
+ }
+
+ function Conversar () {  
+
+    let nome = window.prompt(` Ohh Qual seu nome?`) 
+    window.alert(`Olá ${nome} Prazer conhecer voce. meu nome é: Xadson Lázaro..`) 
+
+    let question = window.confirm(`${nome} Voce Está gostando do site?`) 
+
+    if (question === true) { 
+        window.alert(`Que bom que esta Gostando ${nome} fiz com muito Carrinho`)  
+
+        let questions = window.prompt(` ${nome} Neste site uque pode ser melhorado? Fica a vontade!`) 
+
+        window.confirm(`Sim boa observação: melhorar mais O ${questions} seria muito bom. ${nome} Obrigado pela opinião..`) 
+
+        window.alert(`Sim sei que voce pode estar se perguntando uque isso vai somar na minha vida.. sim ${nome} voce pode sei la dar prá sua finha(a) ou irmão (a) isso vai ajudar muito. então clona ele e coloca prá os Mais novos Brincarem ai em casa. alem disso voce pode ate melhorar ele vai ${nome}`)
+
+         } else { 
+            if (question === false) { 
+                window.alert(` Ohh Por essa Não esperava ${nome} fico feliz em saber que Deixou sua humilde Opinião`) 
+
+                let question2 = window.prompt(`Mé desculpa ${nome} Gostaria de Saber onde posso dar uma melhorada!`) 
+
+                window.confirm(`Haa Nisso ${question2} Sim boa observação ${nome} Vou ver se dou uma melhorada.... (Obrigado)`)
+            }
+    }
+    window.console("kjhfjkd")
+  }
